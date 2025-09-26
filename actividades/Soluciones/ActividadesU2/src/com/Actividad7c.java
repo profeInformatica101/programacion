0fd9d61806bd501e972cf7d7e3313cdfca498655
Pablo <jdommay863@g.educaand.es>
2025-09-26 18:54:59 +0200

    nuevas actividades

--- actividades/Soluciones/ActividadesU2/src/com/Actividad7c.java
+++ actividades/Soluciones/ActividadesU2/src/com/Actividad7c.java
@@ -11,7 +11,7 @@ public class Actividad7c {
 		Scanner sc = new Scanner(System.in);
 		// Definimos las variables
 		double num1, num2, num3;
-		double mayor, medio, menor;
+		double mayor = -1, medio =-1, menor=-1;
 	
 		System.out.println("Dime un número");
 		num1 = sc.nextDouble();
@@ -23,15 +23,41 @@ public class Actividad7c {
 		
 		if((num1 > num2) && (num2 > num3) && (num1 > num3)) {
 			mayor = num1;
-			medio = num2;
+			medio = num2;   /// num1 >> num2 >> num3
 			menor = num3;
-			System.out.println("Sus números ordenados serían "+ mayor + ">" + medio + ">" + menor);
+			
+		//	System.out.println("Sus números ordenados serían "+ mayor + ">" + medio + ">" + menor);
 		}else if((num2 > num1) && (num2 > num3) && (num3 > num1)) {
 			mayor = num2;
-			medio = num3;
+			medio = num3;   // num2 >> num3 >> num1
 			menor = num1;
-			System.out.println("Sus números ordenados serían "+ mayor + ">" + medio + ">" + menor);
+			//System.out.println("Sus números ordenados serían "+ mayor + ">" + medio + ">" + menor);
 		}
+		else if((num3 > num1) && (num3 > num2) && (num2 > num1)) {
+			mayor = num3;
+			medio = num2;   /// num3 >> num2 >> num1
+			menor = num1;
+			//System.out.println("Sus números ordenados serían "+ mayor + ">" + medio + ">" + menor);
+		}else if((num3 > num1) && (num3 > num2) && (num1 > num2)) {
+			mayor = num3;      // 10     6      3
+			medio = num1;    // num3 >> num1 >> num2
+			menor = num2;
+			//System.out.println("Sus números ordenados serían "+ mayor + ">" + medio + ">" + menor);
+		}else if((num1 > num3) && (num1 > num2) && (num3 > num2)) {
+			mayor = num1;    //  10      5       1
+			medio = num3;    // num1 >> num3 >> num2
+			menor = num2;
+			//System.out.println("Sus números ordenados serían "+ mayor + ">" + medio + ">" + menor);
+		}else if((num2 > num1) && (num2 > num3) && (num1 > num3)) {
+			mayor = num2;     //  5      2        1
+			medio = num1;    // num2 >> num1 >> num3
+			menor = num3;
+			//System.out.println("Sus números ordenados serían "+ mayor + ">" + medio + ">" + menor);
+		}else {
+			System.out.println("otro");
+		}
+		
+		System.out.println("Sus números ordenados serían "+ mayor + ">" + medio + ">" + menor);
 	
 	}
 	
